81942ccd3eaeaf55e2baa3a9e93dd22ae6ec9839
WahaaJAli <73385761+WahaaJAli@users.noreply.github.com>
2025-02-17 17:18:25 +0500

    Registration Update

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -6,9 +6,10 @@ const Registration = () => {
 	const [ addError, setAddError ] = useState<string>()
 
   const createUser = ({username, email, password}: IUser): void => {
-    const user: Omit<IUser, '_id'> = {username, email, password}
+    type UserPayload = Omit<IUser, '_id'>
+    const user: UserPayload = {username, email, password}
 
-    RegistrationService.create<Omit<IUser, '_id'>>(user)
+    RegistrationService.create<UserPayload>(user)
       .then(({data: newUser}) => setAddError(''))
       .catch(error => setAddError(error.response.data.message || error.message))
   }
--- src/components/Registration/RegistrationList.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RegistrationList = () => {
-  return (
-    <div>RegistrationList</div>
-  )
-}
-
-export default RegistrationList
